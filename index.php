3595410d4ef3b19efe25a316a28293e3e3b291c0
Tomasz Wielechowski <tomaszwielechowski@gmail.com>
2014-11-18 22:46:22 +0100

    Drugi Commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "To jest właśnie PHP";
+        echo "To jest właśnie PHP. A to jest znowu PHP";
         ?>
     </body>
 </html>
