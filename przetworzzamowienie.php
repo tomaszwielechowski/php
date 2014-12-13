8439fadaa67f10ec97a62ca98879e8e8b32e6407
Tomasz Wielechowski <tomaszwielechowski@gmail.com>
2014-12-13 21:32:33 +0100

    formularz

---
+++ przetworzzamowienie.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Części samochodowe Tomka - wynik i zamówienia</title>
+</head>
+<body>
+<hl>Części samochodowe Tomka</hl>
+<h2>Wyniki zamówienia </h2>
+<?php echo "<p>Zamówienie przyjęte.</p>"; ?>
+</body>
+</html>
+
+
